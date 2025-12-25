02d56eb3070b4aab3760b137f95e34c90a374275
LostScriptures <lost.player.game@gmail.com>
2025-12-25 03:48:25 +0100

    created a testing framework for normal, integration and should_panic tests, made a wrapper for the VGA output (global println and serial_println macros), access to uart for serial

--- kernel/tests/basic_boot.rs
+++ kernel/tests/basic_boot.rs
@@ -4,28 +4,23 @@
 #![test_runner(kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use kernel::println;
 use core::panic::PanicInfo;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     kernel::test_panic_handler(info);
+}
+
+use kernel::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
